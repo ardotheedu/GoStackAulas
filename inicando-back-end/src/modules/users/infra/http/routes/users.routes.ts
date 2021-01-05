5847f7370169927f1dda9fa29616980613b3427e
ardotheedu <eduardosalima3@gmail.com>
2021-01-05 10:43:26 -0300

    refactor: Controllers added

--- inicando-back-end/src/modules/users/infra/http/routes/users.routes.ts
+++ inicando-back-end/src/modules/users/infra/http/routes/users.routes.ts
@@ -3,48 +3,19 @@ import {container} from 'tsyringe'
 import multer from 'multer'
 import uploadConfig from '@config/upload'
 
-import CreateUserService from '@modules/users/services/CreateUserService'
-import UpdateUserAvatarService from '@modules/users/services/UpdateUserAvatarService'
+import UsersController from '../controllers/UserController'
+import UserAvatarController from '../controllers/UserAvatarController'
 
 import ensureAuthenticated from '../middlewares/ensureAuthenticate'
 
 const usersRouter = Router()
+const usersController = new UsersController()
+const userAvatarController = new UserAvatarController()
+
 const upload = multer(uploadConfig)
 
-usersRouter.post('/', async (request, response) => {
-  try {
-    const {name, email, password} = request.body
-    const createUser = container.resolve(CreateUserService)
-    const user = await createUser.execute({
-        name,
-        email,
-        password
-    })
-
-    return response.json(user)
-    } catch (err) {
-        return response.status(400).json({error: err.message})
-    }
-})
+usersRouter.post('/', usersController.create)
 
 usersRouter.patch('/avatar', ensureAuthenticated, upload.single('avatar'),
-async (request, response) => {
-    const updateUserAvatar = container.resolve(UpdateUserAvatarService)
-
-    const user = await updateUserAvatar.execute({
-        user_id: request.user.id,
-        avatarFilename: request.file.filename,
-
-    })
-    const userWhitoutPassword = {
-        id: user.id,
-        name: user.name,
-        email: user.email,
-        created_at: user.created_at,
-        updated_at: user.updated_at,
-        avatar: user.avatar
-    }
-    return response.json(userWhitoutPassword)
-
-})
+userAvatarController.update)
 export default usersRouter
