4d2600563cc4b613aefa70c3fd0120ebeb53a80c
ardotheedu <eduardosalima3@gmail.com>
2021-01-13 16:31:02 -0300

    feat: book an appoinment rules

--- inicando-back-end/src/modules/appointments/infra/typeorm/entities/Appointments.ts
+++ inicando-back-end/src/modules/appointments/infra/typeorm/entities/Appointments.ts
@@ -1,37 +1,42 @@
-import {Entity, 
-    Column, 
-    PrimaryGeneratedColumn, 
-    CreateDateColumn, 
-    UpdateDateColumn, 
-    ManyToOne,
-    JoinColumn} from 'typeorm'
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+  ManyToOne,
+  JoinColumn,
+} from 'typeorm';
 
-import User from '@modules/users/infra/typeorm/entities/User'
+import User from '@modules/users/infra/typeorm/entities/User';
+
 @Entity('appointments')
 class Appointment {
-    //Aqui vai definir os tipos de dados
-    @PrimaryGeneratedColumn('uuid')
-    id: string;
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
 
-    @Column()
-    provider_id: string;
+  @Column()
+  provider_id: string;
 
-    @ManyToOne(() => User) // -> MODEL QUE ELE DEVE RETORNA QUANDO
-    // ESSA FUNÇÃO FOR CHAMADA
-    @JoinColumn({ name: 'provider_id' })
-    // -> qual q coluna que vai identificar qual o prestador desse agendamento aqui
-    provider: User;
-    // Usamos isso por que depois conseguimos os dados completos do usuario
-    // como nome, email diretamente a partir de uym agendamento
+  @ManyToOne(() => User)
+  @JoinColumn({ name: 'provider_id' })
+  provider: User;
 
-    @Column('timestamp with time zone')
-    date: Date;
-    
-    @CreateDateColumn()
-    created_at: Date
+  @Column()
+  user_id: string;
 
-    @UpdateDateColumn()
-    updated_at: Date
+  @ManyToOne(() => User)
+  @JoinColumn({ name: 'user_id' })
+  user: User;
+
+  @Column('timestamp with time zone')
+  date: Date;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
 }
 
 export default Appointment;
--- inicando-back-end/src/modules/appointments/services/CreateAppointmentService.ts
+++ inicando-back-end/src/modules/appointments/services/CreateAppointmentService.ts
@@ -1,40 +1,59 @@
-import {startOfHour} from 'date-fns'
-import {injectable, inject} from 'tsyringe'
+import { startOfHour, isBefore, getHours } from 'date-fns';
+import { injectable, inject } from 'tsyringe';
 
-import AppError from '@shared/errors/AppError'
+import AppError from '@shared/errors/AppError';
 
-import Appointment from '../infra/typeorm/entities/Appointments'
-import IAppointmentRepository from '../repositories/iAppointmentsRepository'
+import Appointment from '../infra/typeorm/entities/Appointments';
+import IAppointmentRepository from '../repositories/iAppointmentsRepository';
 
 interface IRequest {
-    date: Date;
-    provider_id: string;
+  date: Date;
+  provider_id: string;
+  user_id: string;
 }
 @injectable()
 class CreateAppointmentService {
-    constructor(
-      @inject('AppointmentsRepository')
-      private appointmentsRepository: IAppointmentRepository,
-    ) {}
+  constructor(
+    @inject('AppointmentsRepository')
+    private appointmentsRepository: IAppointmentRepository,
+  ) {}
 
-    public async execute({date, provider_id}: IRequest): Promise<Appointment> {
+  public async execute({
+    date,
+    provider_id,
+    user_id,
+  }: IRequest): Promise<Appointment> {
+    const appointmentDate = startOfHour(date);
 
-        const appointmentDate = startOfHour(date)
-
-        const findAppointmentInSameDate = await this.appointmentsRepository.findByDate(
-            appointmentDate
-        )
-        if (findAppointmentInSameDate){
-            throw new AppError("This appointment is already booked");
-        }
-
-        const appointment = await this.appointmentsRepository.create({
-            provider_id, // -> provider: provider,
-            date: appointmentDate
-        })
-
-        return appointment;
+    if (isBefore(appointmentDate, Date.now())) {
+      throw new AppError("You can't create an appointment on a past date");
     }
+
+    if (user_id === provider_id) {
+      throw new AppError("You can't create an appointment with yourself");
+    }
+
+    if (getHours(appointmentDate) < 8 || getHours(appointmentDate) > 17) {
+      throw new AppError(
+        'You can only create appointments between 8am and 5pm',
+      );
+    }
+
+    const findAppointmentInSameDate = await this.appointmentsRepository.findByDate(
+      appointmentDate,
+    );
+    if (findAppointmentInSameDate) {
+      throw new AppError('This appointment is already booked');
+    }
+
+    const appointment = await this.appointmentsRepository.create({
+      provider_id,
+      user_id,
+      date: appointmentDate,
+    });
+
+    return appointment;
+  }
 }
 
 export default CreateAppointmentService;
