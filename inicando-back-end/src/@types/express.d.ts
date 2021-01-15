1a9c3ccf572c2c59b87a73ff1c580f7800292404
Eduardo de Sá <63423067+ardotheedu@users.noreply.github.com>
2021-01-15 16:48:28 -0300

    Delete inicando-back-end directory

--- inicando-back-end/src/@types/express.d.ts
+++
@@ -1,8 +0,0 @@
-declare namespace Express {
-    export interface Request {
-        user: {
-            id: string
-            // -> vai adicionar o user ao Request do Express
-        }
-    }
-}
--- inicando-back-end/src/config/auth.ts
+++
@@ -1,6 +0,0 @@
-export default {
-    jwt: {
-        secret: '139710db2f911b43ceb490cd1ce2bfa7',
-        expiresIn: '1d'
-    }
-}
