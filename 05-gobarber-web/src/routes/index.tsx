2eae9ddf7a6fd27b534ec283ba165feb9e86cafb
ardotheedu <eduardosalima3@gmail.com>
2021-01-21 15:19:22 -0300

    feat: recover password

--- 05-gobarber-web/src/routes/index.tsx
+++ 05-gobarber-web/src/routes/index.tsx
@@ -3,12 +3,17 @@ import { Switch, Route } from 'react-router-dom';
 
 import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
+import ForgotPassword from '../pages/ForgotPassword';
+import ResetPassword from '../pages/ResetPassword';
 import Dashboard from '../pages/Dashboard';
 
 const Routes: React.FC = () => (
   <Switch>
     <Route path="/" exact component={SignIn} />
     <Route path="/signup" exact component={SignUp} />
+    <Route path="/forgot-password " exact component={ForgotPassword} />
+    <Route path="/reset-password " exact component={ResetPassword} />
+
     <Route path="/signup" exact component={Dashboard} isPrivate />
   </Switch>
 );
